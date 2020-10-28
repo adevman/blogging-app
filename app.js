19535a4348606f2023595a3fa7511f4432cbb321
Aman <aman.srivastava262@gmail.com>
2020-10-28 22:06:41 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var expressSanitizer = require("express-sanitizer")
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
+var app = express();
+
+mongoose.connect("mongodb://localhost/restfull_blog_app",{useNewUrlParser:true,useUnifiedTopology:true});
+app.set("view engine","ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(expressSanitizer());
+app.use(methodOverride('_method'));
+
+
+var blogSchema = new mongoose.Schema({
+    title:String,
+    image:String,
+    body:String,
+    created:{type:Date,default:Date.now}
+})
+var Blog = mongoose.model("Blog",blogSchema);
+
+// Blog.create({
+//     title:"Dog",
+//     image:"https://www.sciencemag.org/sites/default/files/styles/article_main_large/public/dogs_1280p_0.jpg?itok=cnRk0HYq",
+//     body:"fur"
+// })
+
+
+//Restfull Routing
+app.get('/',(req,res)=>{
+    res.redirect('/blogs')
+})
+
+//INDEX
+app.get('/blogs',(req,res)=>{
+    Blog.find({},(err,allBlog)=>{
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index",{allBlog:allBlog});
+        }
+    })
+})
+
+//NEW
+app.get('/blogs/new',(req,res)=>{
+    res.render('new');
+})
+
+app.listen('4030',function(req,res){
+    console.log("SERVER IS IN GOOD CONDITION");
+})
